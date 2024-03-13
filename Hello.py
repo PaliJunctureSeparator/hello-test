a6a1b5373d2fb16db2f08d69244518df7f980e9c
PaliJunctureSeparator <162350300+PaliJunctureSeparator@users.noreply.github.com>
2024-03-13 07:10:13 +0000

    change title

--- Hello.py
+++ Hello.py
@@ -20,8 +20,8 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="Pāḷi Text Juncture Separator",
+        page_icon="🌴",
     )
 
     st.write("# Welcome to Pali! 👋")
